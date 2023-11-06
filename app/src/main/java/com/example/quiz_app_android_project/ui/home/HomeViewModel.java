bfd357c0fc5d0190800c480bbc12b30106a58882
harap <harapriyapakal6@gmail.com>
2023-11-06 11:20:26 +0530

    This is our Quiz app project

---
+++ app/src/main/java/com/example/quiz_app_android_project/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.quiz_app_android_project.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/com/example/quiz_app_android_project/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.quiz_app_android_project.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
